878645626ed6bccf84d1a859b0113968f9230c46
Mae <mae@weatherelectric.xyz>
2024-04-24 15:10:09 -0700

    Update download URL

--- Main.cs
+++ Main.cs
@@ -9,7 +9,7 @@ public class Main : MelonMod
     internal const string Author = "SoulWithMae";
     internal const string Company = "Weather Electric";
     internal const string Version = "1.0.0";
-    internal const string DownloadLink = null;
+    internal const string DownloadLink = "https://thunderstore.io/c/bonelab/p/SoulWithMae/MonoDirectorOBS/";
 
     public override void OnInitializeMelon()
     {
