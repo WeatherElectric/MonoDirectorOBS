92174c11483e4308cfd292402255773d5e36c05a
Mae <mae@weatherelectric.xyz>
2024-04-24 15:14:24 -0700

    Removed BoneLib as a referenced assembly, it's not used

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,7 +1,4 @@
 global using System;
 global using System.Reflection;
-global using BoneLib;
 global using MelonLoader;
 global using WeatherElectric.MonoDirectorOBS.Melon;
-global using UnityEngine;
-global using Object = UnityEngine.Object;
